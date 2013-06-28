4390cf2c36e0f0c7b93f4dd464590b3115ce7bd3
Thomas Levine <occurrence@thomaslevine.com>
2013-06-28 12:30:59 -0700

    function annotation for query

--- treasuryio/query.py
+++ treasuryio/query.py
@@ -5,6 +5,14 @@ from urllib import urlencode
 from pandas import DataFrame
 
 def query(sql):
+    '''
+    Submit an `sql` query (string) to treasury.io and return a pandas DataFrame.
+
+    For example::
+
+        print('Operating cash balances for May 22, 2013')
+        print(treasury.io('SELECT * FROM "t1" WHERE "date" = \'2013-05-22\';'))
+    '''
     url = 'https://premium.scraperwiki.com/cc7znvq/47d80ae900e04f2/sql/'
     query_string = urlencode({'q':sql})
     handle = urlopen(url + '?' + query_string)
