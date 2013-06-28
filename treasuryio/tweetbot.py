f4d60cd324892b07506e7fd430c476eca5a70cdb
Thomas Levine <occurrence@thomaslevine.com>
2013-06-28 12:08:44 -0700

    twitter function

---
+++ treasuryio/tweetbot.py
@@ -0,0 +1,41 @@
+import tweepy
+import math
+import re, sys, yaml
+from datetime import datetime
+from json import load
+from urllib2 import urlopen
+from urllib import urlencode
+from pandas import DataFrame
+from query import query
+
+def _connect_to_twitter(config="api.yml"):
+    conf = yaml.safe_load(open(config))
+    auth = tweepy.OAuthHandler(conf['consumer_key'], conf['consumer_secret'])
+    auth.set_access_token(conf['access_token'], conf['access_token_secret'])
+    api = tweepy.API(auth)
+    return api
+
+def tweet(tweet_text_func):
+    '''
+    A decorator to make a function Tweet
+
+    Parameters
+
+    - `tweet_text_func` is a function that takes no parameters and returns a tweetable string
+
+    For example::
+
+        @tweet
+        def total_deposits_this_week():
+            # ...
+
+        @tweet
+        def not_an_interesting_tweet():
+            return 'This tweet is not data-driven.'
+    '''
+    api = _connect_to_twitter()
+    tweet = tweet_text_func()
+    print "Tweeting: %s" % tweet
+    api.update_status(tweet)
+    return tweet
+
